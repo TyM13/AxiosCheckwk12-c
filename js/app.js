249c503ca5abf354478440a480105b01468f9ca6
Your Name <you@example.com>
2022-07-28 18:37:38 -0600

    axioscheckwk12

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function successlink(response){
+document.body.insertAdjacentHTML(`afterbegin`, `
+<h1>success!</h1>
+<h1>${response[`data`][`meals`][0][`strMeal`]}</h1>
+<p>${response[`data`][`meals`][0][`strArea`]}</p>
+<p>${response[`data`][`meals`][0][`strInstructions`]}</p>
+
+`)
+
+}
+
+
+function failurelink (error){
+    document.body.insertAdjacentHTML(`afterbegin`, `
+    <h1>sorry try again</h1>
+    `
+    )
+}
+
+
+axios.request({
+  url: `https://www.themealdb.com/api/json/v1/1/random.php`
+}).then(successlink).catch(failurelink);
