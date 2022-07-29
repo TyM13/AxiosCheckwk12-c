eab8fbcd1cd95f31b99837f85056e05b6a9035d0
Your Name <you@example.com>
2022-07-28 19:57:19 -0600

    axios checkwk12-4

--- js/app.js
+++ js/app.js
@@ -30,32 +30,80 @@
 
 
 
-function successlinktwo(response){
-for (let i = 0; i < response[`data`][`meals`].length; i++){
-    document.body.insertAdjacentHTML(`afterbegin`, `
-    <h1>success!</h1>
-<section>
-<h1>${response[`data`][`meals`][i][`strMeal`]}</h1>
-<img src="${response[`data`][`meals`][i][`strMealThumb`]}">
-</section>
+// function successlinktwo(response){
+// for (let i = 0; i < response[`data`][`meals`].length; i++){
+//     document.body.insertAdjacentHTML(`afterbegin`, `
+//     <h1>success!</h1>
+// <section>
+// <h1>${response[`data`][`meals`][i][`strMeal`]}</h1>
+// <img src="${response[`data`][`meals`][i][`strMealThumb`]}">
+// </section>
 
-    `)
-    }
-    }
-    
-    
-    
-    function failurelinktwo (error){
-        document.body.insertAdjacentHTML(`afterbegin`, `
-        <h1>sorry try again</h1>
-        `
-        )
-    }
-    
-    
+//     `)
+//     }
+//     }
+
+
+
+//     function failurelinktwo (error){
+//         document.body.insertAdjacentHTML(`afterbegin`, `
+//         <h1>sorry try again</h1>
+//         `
+//         )
+//     }
+
+
+//     axios.request({
+//       url: `https://www.themealdb.com/api/json/v1/1/filter.php`,
+//       params: {
+//         c: `Chicken`
+//       }
+//     }).then(successlinktwo).catch(failurelinktwo);
+
+
+
+
+
+
+
+
+
+function successone(response) {
+
+
+
+}
+
+
+
+function failureone(error) {
+    document.body.insertAdjacentHTML(`afterbegin`, `
+                <h1>sorry try again</h1>
+                `
+    )
+}
+
+
+
+
+let post_title =    document.getElementById(`post_title`);
+let title_value = post_title[`value`];
+
+
+let post_body = document.getElementById(`post_body`);
+let body_value = post_body[`value`];
+
+let user_id = document.getElementById(`user_id`);
+let id_value = user_id[`value`];
+
     axios.request({
-      url: `https://www.themealdb.com/api/json/v1/1/filter.php`,
-      params: {
-        c: `Chicken`
-      }
-    }).then(successlinktwo).catch(failurelinktwo);
+        url: `https://jsonplaceholder.typicode.com/posts`,
+        method: `POST`,
+        data: {
+
+            title: title_value,
+            body: body_value,
+            userId: id_value
+
+        }
+    }).then(successone).catch(failureone);
